db57c90666c81fe0dd3914b8af14528927b151a1
T360-v2-DTPB-s09 <marton.szabo@telekom.com>
2022-03-02 16:50:57 +0100

    igen

---
+++ month.py
@@ -0,0 +1,4 @@
+j = 1
+for i in ["Január", "Február", "Március"]:
+    print("A hónap " + str(j) + ". hónapja a " + i)
+    j = j+1
--- while.py
+++ while.py
@@ -16,10 +16,12 @@
 # print("End")
 
 
-number = 1
-while number!=0:
-    number = input("Adj meg egy számot, én meg duplázom. Exit = 0. Számod: ")
-    if(int(number)==0):
-        break
-    print("Duplája: " + str(int(number)*2))
-print("End")
+# number = 1
+# while number!=0:
+#     number = input("Adj meg egy számot, én meg duplázom. Exit = 0. Számod: ")
+#     if(int(number)==0):
+#         break
+#     print("Duplája: " + str(int(number)*2))
+# print("End")
+
+
