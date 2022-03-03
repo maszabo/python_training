ddf9d614c5c25819ad356e06abf1fb69b88fbbc7
T360-v2-DTPB-s09 <marton.szabo@telekom.com>
2022-03-03 11:57:09 +0100

    igen

--- number_guess.py
+++ number_guess.py
@@ -3,7 +3,7 @@ print("Gondolj egy számra 1 és 100 között, én kitalálom.")
 interval_start = 1
 interval_end = 100
 bigger_or_lower = "a"
-
+guess = ""
 while bigger_or_lower != "E":
     guess = int((interval_start+interval_end) / 2)
     bigger_or_lower = input("A szám amire gondoltál Nagyobb [N] / Kisebb [K] / Egyenlő [E] mint " + str(guess) + " ? - ")
